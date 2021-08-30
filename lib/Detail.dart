ac70f892942c87b6e8a2b6cbd0f8eb1234204ef9
FlutLab.io <>
2021-08-30 06:19:38 +0000

    Initial commit

---
+++ lib/Detail.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Detail extends StatelessWidget {
+  final String hospitalName;
+  Detail(this.hospitalName);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Details"),
+      ),
+      body: new Column(
+        children: <Widget>[
+          new Text("${hospitalName}"),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'Detail.dart';
+
+void main() {
+  runApp(new MaterialApp(home: new HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  List data;
+
+  Future<String> getData() async {
+    var response = await http.get(Uri.encodeFull("https://vs0syenr45.execute-api.ap-southeast-1.amazonaws.com/dev/hospitals"), headers: {
+      "Accept": "application/json",
+      "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+
+      //"Access-Control-Allow-Headers": "Access-Control-Allow-Origin, Accept"
+    });
+
+    this.setState(() {
+      data = json.decode(response.body);
+    });
+
+    print(data[1]["name"]);
+
+    return "Success!";
+  }
+
+  @override
+  void initState() {
+    this.getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Listviews"), backgroundColor: Colors.blue),
+      body: new ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40), // if you need this
+              side: BorderSide(
+                color: Colors.grey.withOpacity(0.2),
+                width: 1,
+              ),
+            ),
+            child: GestureDetector(
+              child: new Text(data[index]["name"]),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Detail(data[index]["name"])),
+                );
+              },
+            ),
+          );
+
+          //  );
+        },
+      ),
+    );
+  }
+}
