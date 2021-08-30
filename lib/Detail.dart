378f706b36d05415f7c1bdf834ef3dacd7478d78
FlutLab.io <>
2021-08-30 09:01:44 +0000

    Autosave

--- lib/Detail.dart
+++ lib/Detail.dart
@@ -8,14 +8,18 @@ class Detail extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Details"),
+        title: new Text("Hospital Details"),
       ),
       body: new Column(
         children: <Widget>[
           Container(
             margin: EdgeInsets.all(30),
             padding: EdgeInsets.all(20),
-            child: new Text("${hospitalName}", style: TextStyle(fontSize: 20.0)),
+            child: new Text(
+              "${hospitalName}",
+              style: TextStyle(fontSize: 20.0),
+              textAlign: TextAlign.center,
+            ),
           )
         ],
       ),
