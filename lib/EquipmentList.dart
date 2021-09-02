36912f84f8f88b221edce4093e075b6439248b6c
FlutLab.io <>
2021-09-02 12:05:32 +0000

    Autosave

---
+++ lib/EquipmentList.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EquipmentList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Equipments"),
+      ),
+
+      /// body: new Text("List");
+    );
+  }
+}
