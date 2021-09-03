a6b618c19e67db179b32a077846c366086a2c813
FlutLab.io <>
2021-09-03 04:39:34 +0000

    Autosave

--- lib/Detail.dart
+++ lib/Detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'EquipmentList.dart';
 
 class Detail extends StatelessWidget {
   final String hospitalName;
