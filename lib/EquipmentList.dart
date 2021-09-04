b677cab9ac1743d932012dd62b65071249fee742
FlutLab.io <>
2021-09-04 10:03:35 +0000

    Autosave

--- lib/EquipmentList.dart
+++ lib/EquipmentList.dart
@@ -13,10 +13,19 @@ class EquipmentList extends StatefulWidget {
   EquipmentState createState() => new EquipmentState();
 }
 
+class Equipment {
+  final name;
+  final urgency;
+  final quantity;
+
+  Equipment(this.name, this.urgency, this.quantity);
+}
+
 class EquipmentState extends State<EquipmentList> {
   List data;
+  List<Equipment> equipments;
 
-  Future<String> getData() async {
+  Future<List<Equipment>> getData() async {
     var response = await http.get(Uri.encodeFull("https://myhospitalsapi.aihw.gov.au/api/v0/retired-myhospitals-api/hospitals"), headers: {
       //"Access-Control-Allow-Headers": "Access-Control-Allow-Origin, Accept"
     });
@@ -25,9 +34,12 @@ class EquipmentState extends State<EquipmentList> {
       data = json.decode(response.body);
     });
 
+    for (var i = 0; i < data.length; i++) {
+      equipments.add(new Equipment(data[i]["name"], data[i]["isPublic"], data[i]["code"]));
+    }
     print(data[1]["name"]);
 
-    return "Success!";
+    return equipments;
   }
 
   @override
@@ -40,13 +52,38 @@ class EquipmentState extends State<EquipmentList> {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Required Equipments"), backgroundColor: Colors.blue),
       body: new ListView.builder(
-        itemCount: data == null ? 0 : data.length,
+        itemCount: equipments == null ? 0 : equipments.length,
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            //leading: icon,
-            title: Text(data[index]["name"]),
-            //subtitle: "Urgent",
+          return Container(
+            height: 150,
+            child: Card(
+              color: Colors.orange,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 33,
+                    child: Image.network(
+                      'https://picsum.photos/250?image=9',
+                    ),
+                  ),
+                  Expanded(
+                    flex: 66,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          flex: 50,
+                          child: Center(child: Text(equipments[index].name)),
+                        ),
+                        Expanded(flex: 25, child: Text(equipments[index].urgency)),
+                        Expanded(flex: 25, child: Text(equipments[index].quantity)),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           );
+
           // return Card(
           //   shape: RoundedRectangleBorder(
           //     borderRadius: BorderRadius.circular(40), // if you need this
