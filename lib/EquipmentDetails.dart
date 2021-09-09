cde18255e6f6d6c71b66a41304c6c61b1986d9d8
FlutLab.io <>
2021-09-09 12:29:15 +0000

    Autosave

--- lib/EquipmentDetails.dart
+++ lib/EquipmentDetails.dart
@@ -102,25 +102,46 @@ class EquipmentDetails extends StatelessWidget {
               ),
             ),
           ]),
-          Container(
-            height: 50.0,
-            margin: EdgeInsets.all(10),
-            //  padding: EdgeInsets.all(30),
-            width: double.infinity,
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+            Container(
+              height: 50.0,
+              margin: EdgeInsets.all(10),
+              //  padding: EdgeInsets.all(30),
+              width: double.infinity,
 
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EquipmentList(hospitalName)),
-                );
-              },
-              child: Text(
-                'Edit',
-                style: TextStyle(fontSize: 25.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => EquipmentList(hospitalName)),
+                  );
+                },
+                child: Text(
+                  'Edit',
+                  style: TextStyle(fontSize: 25.0),
+                ),
               ),
             ),
-          ),
+            Container(
+              height: 50.0,
+              margin: EdgeInsets.all(10),
+              //  padding: EdgeInsets.all(30),
+              width: double.infinity,
+
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => EquipmentList(hospitalName)),
+                  );
+                },
+                child: Text(
+                  'Delete',
+                  style: TextStyle(fontSize: 25.0),
+                ),
+              ),
+            ),
+          ]),
         ],
       ),
     );
