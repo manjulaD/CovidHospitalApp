300e0168b2b4e0293d85677c1159ec6c7cfd7f92
FlutLab.io <>
2021-09-30 18:03:15 +0000

    Autosave

--- lib/RequiredEquipmentDetails.dart
+++ lib/RequiredEquipmentDetails.dart
@@ -16,6 +16,63 @@ class RequiredEquipmentDetails extends StatelessWidget {
       ),
       body: new Column(
         children: <Widget>[
+          //
+
+          Container(
+            // color: Colors.green,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            child: new Text(
+              equipment.name,
+              style: TextStyle(fontSize: 25.0),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Column(children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(5),
+              padding: EdgeInsets.all(5),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+                Flexible(
+                    child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+                  Text("Very Urgent", style: new TextStyle(color: Colors.red, fontSize: 13.0)),
+                  Text(equipment.veryUrgent, style: new TextStyle(color: Colors.redAccent, fontSize: 16.0)),
+                ])),
+                Flexible(
+                    child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+                  Text("Urgent", style: new TextStyle(color: Colors.orange, fontSize: 13.0)),
+                  Text(equipment.urgent, style: new TextStyle(color: Colors.orangeAccent, fontSize: 16.0)),
+                ])),
+                Flexible(
+                    child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+                  Text("Regular Needs", style: new TextStyle(color: Colors.blue, fontSize: 13.0)),
+                  Text(equipment.regularNeeds, style: new TextStyle(color: Colors.blueAccent, fontSize: 16.0)),
+                ])),
+              ]),
+            ),
+            Container(
+                margin: EdgeInsets.all(5),
+                padding: EdgeInsets.all(5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Flexible(
+                        child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+                      Text("Current At Hand", style: new TextStyle(color: Colors.indigo, fontSize: 13.0)),
+                      Text(equipment.currentAtHandQuantity, style: new TextStyle(color: Colors.indigo, fontSize: 16.0)),
+                    ])),
+
+                    Flexible(
+                        child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+                      Text("Excess Qty", style: new TextStyle(color: Colors.green, fontSize: 13.0)),
+                      Text(equipment.excessQuantity, style: new TextStyle(color: Colors.green, fontSize: 16.0)),
+                    ])),
+                    //onPressed: moveToRegister,
+                  ],
+                ))
+          ]),
+
+          //
           Row(children: <Widget>[
             Container(
               // color: Colors.green,
